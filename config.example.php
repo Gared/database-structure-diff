fbc28378268f5af0c28dec3f6e8df85a66f04390
Stefan Mueller <mu.stefan@googlemail.com>
2020-07-31 19:29:50 +0200

    Clarify config example

--- config.example.php
+++ config.example.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-    'diff between sql script and database' => [
+    // diff between sql script and database
+    [
         [
-            'dbname' => 'database_name',
-            'user' => 'username',
-            'password' => 'password',
-            'host' => 'hostname',
-            'driver' => 'pdo_mysql',
+            'dbname' => 'database_name', // Database name
+            'user' => 'username', // user
+            'password' => 'password', // password
+            'host' => 'hostname', // host or ip address
+            'driver' => 'pdo_mysql', // mysql => pdo_mysql, oracle => pdo_oci, ms sql => pdo_dblib, etc. see https://www.php.net/manual/de/pdo.drivers.php
         ],
         [
             'dbname' => 'database_name',
@@ -15,7 +16,8 @@ return [
             'driver' => 'file',
         ],
     ],
-    'diff between two databases' => [
+    // diff between two databases
+    [
         [
             'dbname' => 'database_name',
             'user' => 'username',
